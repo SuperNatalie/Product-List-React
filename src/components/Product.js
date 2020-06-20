a1eb60a2010307f341603e20960daf8f6d87d8de
Natalie <natalie@super-code.de>
2020-06-20 16:28:01 +0200

    add read me

---
+++ src/components/Product.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Product.css";
+
+const Product = (props) => (
+  <div>
+    <div id="box">
+      <img
+        src={props.imagesrc}
+        alt="https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-se-red-select-2020_GEO_EMEA?wid=470&hei=556&fmt=png-alpha&.v=1586574260374"
+      ></img>
+      <h2>{props.title}</h2>
+      <p>{props.price}</p>
+    </div>
+  </div>
+);
+
+export default Product;
